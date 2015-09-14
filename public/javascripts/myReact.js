4653fea9d82c6d71d30e22fe6f6780709c432ad4
WANG Cheng <wcyz666@126.com>
2015-09-13 20:38:00 -0700

    First commit

---
+++ public/javascripts/myReact.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ASUA on 2015/9/13.
+ */
+var QueryResult = React.createClass({
+    render: function () {
+        return (
+            <table>
+                <tr>
+                    <th>
+                       dddd
+                    </th>
+                </tr>
+                <tbody>
+                    ddd
+                </tbody>
+            </table>
+        );
+    }
+});
+React.render(
+    <QueryResult />,
+    document.getElementById('content')
+);
---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require("mysql");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/query', function(req, res, next) {
+    var query = req.body.query.toLowerCase();
+    if (query.indexOf("drop") != -1 || query.indexOf("insert") != -1 || query.indexOf("delete") != -1)
+        res.end("We only support SELECT")
+    else {
+        connection.query(query, function(err, rows, fields) {
+            if (err) throw err;
+            /*var i,
+                scores = [],
+                score;
+            for (i = 0; i < rows.length; i++){
+                score = {
+                    username : rows[i].username,
+                    score : rows[i].score,
+                    rank : i + 1
+                };
+                scores.push(score);
+            }
+            console.log(req.cookies);
+            res.render("scores", {me : username, scores: scores});
+            */
+            res.json({
+                field : fields,
+                rows: rows
+            });
+        });
+    }
+
+});
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : "lab2"
+});
+connection.connect();
+
+
+module.exports = router;
